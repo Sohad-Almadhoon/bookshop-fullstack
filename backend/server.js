15a4ca6aa64d3fef24e9e29687fa28dd600b8e3b
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2024-12-07 12:46:56 +0200

    ...

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ import bodyParser from "body-parser";
 const app = express();
 dotenv.config();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" , credentials : true}));
+app.use(cors());
 app.use(bodyParser.json());
 
 const routes = {
