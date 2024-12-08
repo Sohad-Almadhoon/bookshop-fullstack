f9633ac861f95ea7052618627f40802cd2ad3fe4
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2024-12-08 13:03:36 +0200

    ...

--- frontend/src/utils/newRequest.ts
+++ frontend/src/utils/newRequest.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://bookshop-api-sage.vercel.app"
+    ? "https://bookshop-hanx.onrender.com"
     : "http://localhost:5000";
 
 const newRequest = axios.create({
